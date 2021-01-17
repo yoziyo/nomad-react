54c598780d1f52254495c8be1e78e7c4b908a449
yoooozi <j16412000kr@gmail.com>
2021-01-17 22:17:29 +0900

    CSS in React part Three

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,8 @@
 import Router from 'Components/Router';
-import Header from 'Components/Header';
 
 function App() {
   return (
     <>
-      <Header />
       <Router />
     </>
   );
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,21 +1,25 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
-import Home from "Routes/Home";
-import TV from "Routes/TV";
-import Search from "Routes/Search";
+import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import Home from 'Routes/Home';
+import TV from 'Routes/TV';
+import Search from 'Routes/Search';
+import Header from 'Components/Header';
 
 const router = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/tv" exact component={TV} />
-        <Route path="/tv/popular" render={() => <h1>Popular</h1>} />
-        <Route path="/search" component={Search} />
-        <Redirect from="*" to="/" />
-      </Switch>
+      <>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/tv" exact component={TV} />
+          <Route path="/tv/popular" render={() => <h1>Popular</h1>} />
+          <Route path="/search" component={Search} />
+          <Redirect from="*" to="/" />
+        </Switch>
+      </>
     </Router>
   );
-}
+};
 
 export default router;
