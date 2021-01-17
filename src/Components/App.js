caae56d8f36c2be6c26cca09f71a4d3f824357f5
yoooozi <j16412000kr@gmail.com>
2021-01-17 21:55:48 +0900

    CSS in React part Two

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,5 @@
-import Router from "Components/Router";
-import Header from "Components/Header";
+import Router from 'Components/Router';
+import Header from 'Components/Header';
 
 function App() {
   return (
@@ -8,7 +8,6 @@ function App() {
       <Router />
     </>
   );
-  }
-  
-  export default App;
-  
+}
+
+export default App;
