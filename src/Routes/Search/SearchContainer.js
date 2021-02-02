eabfb82c5a661cdc29611669b7b1c7d09f3cad56
yoooozi <j16412000kr@gmail.com>
2021-02-03 00:33:13 +0900

    Container Presenter Pattern part Two

---
+++ src/Routes/Search/SearchContainer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import SearchPresenter from './SearchPresenter';
+
+export default class SearchContainer extends React.Component {
+  state = {
+    movieResult: null,
+    tvResult: null,
+    searchTerm: '',
+    error: null,
+    loading: false,
+  };
+
+  render() {
+    const { movieResult, tvResult, searchTerm, error, loading } = this.state;
+    return <SearchPresenter movieResult={movieResult} tvResult={tvResult} searchTerm={searchTerm} error={error} loading={loading} />;
+  }
+}
