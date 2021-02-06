102676a702077f981079f02d479221c241b5f58e
yoooozi <j16412000kr@gmail.com>
2021-02-06 22:25:35 +0900

    Detail Container part Two

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,12 +1,21 @@
+import { moviesApi, tvApi } from 'api';
 import React from 'react';
 import DetailPresenter from './DetailPresenter';
 
 export default class DetailContainer extends React.Component {
-  state = {
-    result: '',
-    error: null,
-    loading: true,
-  };
+  constructor(props) {
+    super(props);
+    const {
+      location: { pathname },
+    } = props;
+
+    this.state = {
+      result: '',
+      error: null,
+      loading: true,
+      isMovie: pathname.includes('/movie/'),
+    };
+  }
 
   async componentDidMount() {
     const {
@@ -17,9 +26,24 @@ export default class DetailContainer extends React.Component {
     } = this.props;
 
     const parsedId = Number(id);
-
     if (isNaN(parsedId)) {
       return push('/');
+    }
+
+    const { isMovie } = this.state;
+    let result = null;
+    try {
+      if (isMovie) {
+        const request = await moviesApi.movieDetail(parsedId);
+        result = request.data;
+      } else {
+        const request = await tvApi.showDetail(parsedId);
+        result = request.data;
+      }
+    } catch {
+      this.setState({ error: "Can't find anything." });
+    } finally {
+      this.setState({ loading: false, result });
     }
   }
 
