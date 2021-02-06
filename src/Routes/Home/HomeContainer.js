058c820934fe51da941531567410ed1a70a214c4
yoooozi <j16412000kr@gmail.com>
2021-02-06 18:32:52 +0900

    Home Container

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -1,3 +1,4 @@
+import { moviesApi } from 'api';
 import React from 'react';
 import HomePresenter from './HomePresenter';
 
@@ -9,6 +10,28 @@ export default class HomeContainer extends React.Component {
     error: null,
     loading: true,
   };
+
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: nowPlaying },
+      } = await moviesApi.nowPlaying();
+
+      const {
+        data: { results: upcoming },
+      } = await moviesApi.upcoming();
+
+      const {
+        data: { results: popular },
+      } = await moviesApi.popular();
+
+      this.setState({ nowPlaying, upcoming, popular });
+    } catch {
+      this.setState({ error: "Can't find movies informations." });
+    } finally {
+      this.setState({ loading: false });
+    }
+  }
 
   render() {
     const { nowPlaying, upcoming, popular, error, loading } = this.state;
