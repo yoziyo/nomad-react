fb17e0b952663f60ab7c1bab088f71d1ba3b7b29
yoooozi <j16412000kr@gmail.com>
2021-02-08 23:58:32 +0900

    SearchPresenter Component

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -11,11 +11,19 @@ export default class SearchContainer extends React.Component {
     loading: false,
   };
 
-  handleSubmit = () => {
+  handleSubmit = (event) => {
+    event.preventDefault();
     const { searchTerm } = this.state;
     if (searchTerm !== '') {
       this.searchByTerm();
     }
+  };
+
+  updateTerm = (event) => {
+    const {
+      target: { value },
+    } = event;
+    this.setState({ searchTerm: value });
   };
 
   searchByTerm = async () => {
@@ -48,6 +56,7 @@ export default class SearchContainer extends React.Component {
         error={error}
         loading={loading}
         handleSubmit={this.handleSubmit}
+        updateTerm={this.updateTerm}
       />
     );
   }
--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,14 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Loader from 'Components/Loader';
+import Section from 'Components/Section';
 
-const SearchPresenter = ({ movieResult, tvResult, searchTerm, error, loading, handleSubmit }) => null;
+const Container = styled.div`
+  padding: 0px 20px;
+`;
+
+const Form = styled.form`
+  margin-bottom: 50px;
+  width: 100%;
+`;
+
+const Input = styled.input`
+  all: unset;
+  font-size: 28px;
+  width: 100%;
+`;
+
+const SearchPresenter = ({ movieResult, tvResult, searchTerm, error, loading, handleSubmit, updateTerm }) => (
+  <Container>
+    <Form onSubmit={handleSubmit}>
+      <Input placeholder="Search Movies or TV Show..." value={searchTerm} onChange={updateTerm}></Input>
+    </Form>
+    {loading ? (
+      <Loader />
+    ) : (
+      <>
+        {movieResult && movieResult.length > 0 && (
+          <Section title="Movie Result">
+            {movieResult.map((movie) => (
+              <span key={movie.id}>{movie.title}</span>
+            ))}
+          </Section>
+        )}
+        {tvResult && tvResult.length > 0 && (
+          <Section title="TV Show Result">
+            {tvResult.map((tv) => (
+              <span key={tv.id}>{tv.name}</span>
+            ))}
+          </Section>
+        )}
+      </>
+    )}
+  </Container>
+);
 
 SearchPresenter.propTypes = {
   movieResult: PropTypes.array,
   tvResult: PropTypes.array,
   searchTerm: PropTypes.string,
   handleSubmit: PropTypes.func.isRequired,
+  updateTerm: PropTypes.func.isRequired,
   loading: PropTypes.bool.isRequired,
   error: PropTypes.string,
 };
